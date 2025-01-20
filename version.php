26c92c3f999a887caa0ecaf825876d22a2f6ab40
Dan Marsden <dan@danmarsden.com>
2025-01-20 19:30:31 +1300

    bump version for release.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_subcourse';
-$plugin->release = 2024072400;
-$plugin->version = 2024072400;
+$plugin->release = 2025012000;
+$plugin->version = 2025012000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2024100700; // Requires 4.5.
 $plugin->supported = [405, 405];
